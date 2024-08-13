ded4684bea551dd247e2117e6187db8801d45304
lcchua <laichwang@gmail.com>
2024-08-13 10:56:51 +0800

    Updated main tf file

--- main.tf
+++ main.tf
@@ -12,6 +12,11 @@ variable "stack_name" {
   default = "lcchua-STW"
 }
 
+variable "key_name" {
+  description = "Name of EC2 Key Pair"
+  type        = string
+  default     = "lcchua-useast1-20072024" # Change accordingly
+}
 
 #============ MAIN =============
 
